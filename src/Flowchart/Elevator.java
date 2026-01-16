e9cd689e908acc17b258f9e798b4b1e32e1409f4
Seohana <onnuri1226@gmail.com>
2026-01-16 10:02:36 +0900

    260116 엘레베이터 상대적 위치 판단

---
+++ src/Flowchart/Elevator.java
@@ -0,0 +1,40 @@
+package Flowchart;
+
+import java.util.Scanner;
+
+public class Elevator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==엘레베이터 프로그램 시작==");
+
+        System.out.println("현재 층을 입력해주세요");
+        int floor = Integer.parseInt(sc.nextLine());
+
+        System.out.println("도착 층을 입력해주세요");
+        int arrival = Integer.parseInt(sc.nextLine());
+
+        while (true) {
+            if (floor == 0) {
+                System.out.println("현재 정비중입니다. 조금만 기다려 주세요");
+                break;
+            } else if (floor == arrival) {
+                System.out.println("이미 같은 층입니다.");
+
+                System.out.println("다시 누르시겠습니까?");
+
+                if ("yes".equals(sc.nextLine())) {
+                    sc.next();
+                }
+            } else if (floor < arrival) {
+                System.out.println(arrival + "층으로 올라갑니다");
+                break;
+            } else if (floor > arrival) {
+                System.out.println(arrival + "층으로 내려갑니다");
+                break;
+            }
+        } sc.close();
+    }
+}
