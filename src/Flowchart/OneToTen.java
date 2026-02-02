83b8a79c1d0515caa96a20ce4c20f9f2f59045b4
Seohana <onnuri1226@gmail.com>
2026-02-02 11:01:08 +0900

    feat : 260202 while, for문 1-10까지 출력

---
+++ src/Flowchart/OneToTen.java
@@ -0,0 +1,12 @@
+package Flowchart;
+
+public class OneToTen {
+
+    public static void main(String[] args) {
+        int num = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
